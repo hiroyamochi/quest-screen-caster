e78e02c27b9484c08e2152b7facc38b20fff6248
hiroyamochi <hirodora13@gmail.com>
2024-03-04 13:27:44 +0900

    v1.0

--- Readme.md
+++ Readme.md
@@ -1,26 +1,35 @@
 # Scrcpy GUI for Quest
 
-This is a simple GUI for [Scrcpy](https://github.com/Genymobile/scrcpy), a tool for displaying and controlling Android devices connected via USB or TCP/IP. This GUI is specifically designed for Oculus Quest devices.
+This is a simple GUI for [Scrcpy](https://github.com/Genymobile/scrcpy), a tool for displaying and controlling Android devices connected via USB (currently not supported via TCP/IP). This GUI is specifically designed for Meta Quest devices.
 
 ## Prerequisites
 
 - Python 3
 - [Scrcpy](https://github.com/Genymobile/scrcpy)
 - [ADB](https://developer.android.com/studio/command-line/adb) (Android Debug Bridge)
-- Only for Windows
+- Only works with Windows
 
-Make sure that `scrcpy` and `adb` are located in the `scrcpy` directory in the same directory as this script.
+## Usage
+- Get [Scrcpy](https://github.com/Genymobile/scrcpy)
+- Rename scrcpy folder to `scrcpy` (a folder where scrcpy.exe exists)
+- Download screen-caster-quest.exe from release page
+- Directory should be like below:
+```
+screen-caster-quest
+├ screen-caster-quest.exe
+└ scrcpy
+　 └ scrcpy.exe
+```
 
 ## Build
 - Run following in the project directory:
 ```bash
-pyinstaller --onefile --windowed --add-data "scrcpy;scrcpy" main.py --name screen-caster-quest
+pyinstaller --onefile --windowed --add-data main.py --name screen-caster-quest
 ```
-
 
 ## Configuration
 
-You can set the default bitrate in the `config.ini` file in the same directory as this script. The default bitrate is used when the "Bitrate" field is left empty. The `config.ini` file should look like this:
+You can set the default bitrate in the `config.ini` file in the same directory as this script. The default bitrate is used when the "Bitrate" field is left empty.
 
 ```ini
 [scrcpy]
--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Button
 import subprocess
 import os
 import threading
 import configparser
 import sys
 
-# PyInstaller Support
-base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+
+def find_application_directory():
+    if getattr(sys, 'frozen', False):
+        # If frozen (in built executable), the path to scrcpy.exe is in same directory as executable
+        application_path = sys._MEIPASS
+    else:
+        # If not frozen (dev environment), the path to scrcpy.exe is in the scrcpy directory
+        application_path = os.path.dirname(os.path.abspath(__file__))
+    return application_path
 
 # path to scrcpy.exe
-scrcpy_path = os.path.join(base_path, 'scrcpy', 'scrcpy.exe')
-
-
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.realpath(__file__))
-# scrcpy_dir = os.path.join(bundle_dir, 'scrcpy')
+scrcpy_path = os.path.join(find_application_directory(), "scrcpy", "scrcpy.exe")
 
 # Load config
 def load_config():
     config = configparser.ConfigParser()
-    config.read(os.path.join(script_dir, 'config.ini'))
+    config.read(os.path.join(find_application_directory(), 'config.ini'))
     return config
 
 # Load default bitrate from config
 config = load_config()
 default_bitrate = config.getint('scrcpy', 'bitrate', fallback=20)
+default_size = config.getint('scrcpy', 'size', fallback=1024)
 
 casting_devices = {}
 
@@ -39,22 +42,17 @@ def initialize_adb():
 def start_scrcpy():
     global casting_devices
 
-    # Hide window when starting scrcpy
-    # si = subprocess.STARTUPINFO()
-    # si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    # si.wShowWindow = subprocess.SW_HIDE 
-
     # Set options
-    full_device_info = serial_var.get()
-    serial_id = full_device_info.split(" (")[1].rstrip(")")
+    device_name = serial_var.get().split()[0]
+    serial = device_serials[device_name]
     size = size_entry.get()
     bitrate = bitrate_entry.get() or default_bitrate
     screen_off = screen_off_var.get()
-    title = f"Scrcpy for Quest - {serial_id}"
-    # device = device_type_var.get()
+    title = f"Scrcpy for Quest - {serial}"
+    device_type = device_type_var.get()
 
     # Construct the command
-    command = [scrcpy_path, "-s", serial_id]
+    command = [scrcpy_path, "-s", serial]
     if size:
         command.extend(["--max-size", size])
     if bitrate:
@@ -63,28 +61,32 @@ def start_scrcpy():
         command.append("--power-off-on-close")
     if title:
         command.extend(["--window-title", title])
-    # if device == "Quest 2":
-    #     command.append(["--crop=1450:1450:140:140"])
-    # elif device == "Quest 3":
-    #     command.append(["--crop 2064:2208:2064:100"]) # Consider later
-
-    # Start the process asynchronously and redirect stderr to stdout
+    
+    if device_type == "Quest 2":
+        command.append("--crop=1450:1450:140:140")
+    elif device_type == "Quest 3":
+        command.append("--crop=1650:1650:300:300") # Consider later
+    elif device_type == "Quest Pro":
+        command.append("--crop=2064:2208:2064:100") # Consider later
+    elif device_type == "Other":
+        pass
+        
     process = subprocess.Popen(
         command,
         creationflags=subprocess.CREATE_NO_WINDOW
         )
 
-    casting_devices[serial_id] = process
+    casting_devices[serial] = process
 
     # Start a separate thread to wait for the process to finish and output any errors
-    threading.Thread(target=monitor_casting, args=(serial_id,)).start()
+    threading.Thread(target=monitor_casting, args=(serial,)).start()
 
 def monitor_casting(serial):
     global casting_devices
 
     # Wait for the process to finish
     process = casting_devices[serial]
-    # stdout, _ = process.communicate()  # This will also capture stderr because of the redirection
+    stdout, _ = process.communicate()  # This will also capture stderr because of the redirection
 
     # Print the output, which includes stderr
     # print(stdout.decode())
@@ -97,15 +99,19 @@ def monitor_casting(serial):
 def stop_scrcpy():
     global casting_devices
 
-    selected_device = serial_var.get()
-    if selected_device not in casting_devices:
+    # Get the serial number from the device_serials dictionary
+    device_name = serial_var.get().split()[0]
+    serial = device_serials[device_name]
+    if serial not in casting_devices:
         print("No casting found for the selected device.")
         return
 
-    process = casting_devices[selected_device]
+    process = casting_devices[serial]
     process.terminate()
-    del casting_devices[selected_device]
-    print(f"Casting stopped for device: {selected_device}")
+    del casting_devices[serial]
+    print(f"Casting stopped for device: {device_name}")
+
+device_serials = {}
 
 def get_device_details_async():
     def get_device_details():
@@ -119,25 +125,41 @@ def get_device_details_async():
                 details = " ".join(parts[1:])
                 # Extract the model name
                 model = [s for s in details.split() if "model:" in s][0].replace("model:", "")
-                devices.append(f"{model} ({serial})")
+                devices.append(model)
+                device_serials[model] = serial  # Store the serial number
         
         # Update the dropdown menu with the found devices
-        serial_var.set(devices[0] if devices else "")
+        device_label = f"{devices[0]} ({device_serials[devices[0]]})" if devices else ""
+        serial_var.set(device_label)
         serial_menu["menu"].delete(0, "end")
         for device in devices:
-            serial_menu["menu"].add_command(label=device, command=tk._setit(serial_var, device))
+            serial = device_serials[device]
+            label = f"{device} ({serial})"
+            serial_menu["menu"].add_command(label=label, command=tk._setit(serial_var, label))
         
         get_button.config(state="normal")  # Re-enable the button
-        print(devices)
+        print(device_serials)
 
     get_button.config(state="disabled")  # Disable the button while fetching the devices
     threading.Thread(target=get_device_details).start()
+
+
+def disable_proximity_sensor():
+    device_name = serial_var.get().split()[0]
+    serial = device_serials[device_name]
+    subprocess.run(["adb", "-s", serial, "shell", "am", "broadcast", "-a", "com.oculus.vrpowermanager.prox_close"])
+
+def enable_proximity_sensor():
+    device_name = serial_var.get().split()[0]
+    serial = device_serials[device_name]
+    subprocess.run(["adb", "-s", serial, "shell", "am", "broadcast", "-a", "com.oculus.vrpowermanager.automation_disable"])
+
 
 # Create Tkinter window
 root = tk.Tk()
 root.title("Scrcpy GUI for Quest")
 root.resizable(False, False)  # Disable window resizing
-root.geometry("500x200")  # Set window size
+root.geometry("600x300")  # Set window size
 
 # Set font
 if 'win' in root.tk.call('tk', 'windowingsystem'):
@@ -147,7 +169,7 @@ else:
 
 # Serial number input and Get button
 serial_label = tk.Label(root, text="Device:")
-serial_label.grid(row=0, column=0, sticky=tk.W)
+serial_label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
 serial_var = tk.StringVar()
 serial_menu = tk.OptionMenu(root, serial_var, "")
 serial_menu.grid(row=0, column=1, sticky=tk.EW)
@@ -155,10 +177,11 @@ get_button = tk.Button(root, text="Get", command=get_device_details_async)
 get_button.grid(row=0, column=2)
 
 # Mirroring window size specification
-size_label = tk.Label(root, text="Screen Size (e.g., 1920x1080):")
+size_label = tk.Label(root, text="Screen Size (e.g., 1024):")
 size_label.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
 size_entry = tk.Entry(root, width=15)
 size_entry.grid(row=1, column=1, sticky=tk.EW, padx=5, pady=5)
+size_entry.insert(0, str(default_size))  # Insert default size
 
 # Bitrate specification
 bitrate_label = tk.Label(root, text="Bitrate (Mbps):")
@@ -177,21 +200,34 @@ screen_off_checkbox.grid(row=3, column=0, sticky=tk.W, padx=5, pady=5, columnspa
 start_button = tk.Button(root, text="Start Scrcpy", command=start_scrcpy)
 start_button.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
 stop_button = tk.Button(root, text="Stop Scrcpy", command=stop_scrcpy)
-stop_button.grid(row=4, column=1, padx=5, pady=5, sticky=tk.E)
+stop_button.grid(row=4, column=1, padx=5, pady=5, sticky=tk.W)
 
 # Device Selection
-# device_type_var = tk.StringVar(value="Other")
-# quest_2_radio = tk.Radiobutton(root, text="Quest 2", variable=device_type_var, value="Quest 2")
-# quest_2_radio.grid(row=5, column=0, sticky=tk.W)
-# quest_3_radio = tk.Radiobutton(root, text="Quest 3", variable=device_type_var, value="Quest 3")
-# quest_3_radio.grid(row=5, column=1, sticky=tk.W)
-# quest_pro_radio = tk.Radiobutton(root, text="Quest Pro", variable=device_type_var, value="Quest Pro")
-# quest_pro_radio.grid(row=5, column=2, sticky=tk.W)
-# other_radio = tk.Radiobutton(root, text="Other", variable=device_type_var, value="Other")
-# other_radio.grid(row=5, column=3, sticky=tk.W)
+device_type_var = tk.StringVar(value="Other")
+# device_type_label = tk.Label(root, text="Device Type:")
+# device_type_label.grid(row=5, column=0, sticky=tk.W, padx=5, pady=5)
+quest_2_radio = tk.Radiobutton(root, text="Quest 2", variable=device_type_var, value="Quest 2")
+quest_2_radio.grid(row=5, column=0, sticky=tk.W)
+quest_3_radio = tk.Radiobutton(root, text="Quest 3", variable=device_type_var, value="Quest 3")
+quest_3_radio.grid(row=5, column=1, sticky=tk.W)
+quest_pro_radio = tk.Radiobutton(root, text="Quest Pro", variable=device_type_var, value="Quest Pro")
+quest_pro_radio.grid(row=5, column=2, sticky=tk.W)
+other_radio = tk.Radiobutton(root, text="Other", variable=device_type_var, value="Other")
+other_radio.grid(row=5, column=3, sticky=tk.W)
+
+# Buttons for enabling and disabling the proximity sensor
+proximity_sensor_button = Button(root, text="Enable Proximity Sensor", command=enable_proximity_sensor)
+proximity_sensor_button.grid(row=6, column=0, padx=5, pady=5)
+
+disable_proximity_sensor_button = Button(root, text="Disable Proximity Sensor", command=disable_proximity_sensor)
+disable_proximity_sensor_button.grid(row=6, column=2, padx=5, pady=5)
+
 
 # Adjust the grid configuration
 root.grid_columnconfigure (1, weight=1)
+root.grid_columnconfigure (2, weight=1)
+root.grid_columnconfigure (3, weight=1)
+root.grid_columnconfigure (4, weight=1)
 
 # Initialize ADB in a separate thread to avoid freezing the GUI
 threading.Thread(target=initialize_adb).start()
